56ef12b272f6e05ef714e1317325219392410192
PangVu <104439024+PangVu@users.noreply.github.com>
2022-05-03 21:44:10 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    float r;//半径
+    float pi = 3.1415926535897932384626433832795;
+    printf("please input your radius: \n");
+    scanf("%f",&r);
+    printf("your circle areas is : %f",r*r*pi);
+    printf("\n");
+
+    return 0;
+}
